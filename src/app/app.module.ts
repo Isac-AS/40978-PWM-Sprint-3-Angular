953ea49698096e01bca678d7a248773d934c12fa
isac <isac.as0001@gmail.com>
2022-05-03 08:52:13 +0100

    PreMerge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,7 +54,6 @@ import { HeaderResponsiveDialogComponent } from './components/commons-header/hea
 import { InfoMessagePopupComponent } from './components/info-message-popup/info-message-popup.component';
 import { UserModificationPopupComponent } from './components/user-modification-popup/user-modification-popup.component';
 import { ConcreteProductPageComponent } from './pages/concrete-product-page/concrete-product-page.component';
-
 // @ts-ignore
 @NgModule({
   declarations: [
